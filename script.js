365777b43453aca13d3ee257cb91ad8e04160cea
Steven <stevenkincaid15@gmail.com>
2023-02-27 10:03:15 -0800

    85% complete with foundations

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+const body = document.querySelector('body')
+const results = document.querySelector('.results');
+const scoreBoard = document.createElement('div');
+body.appendChild(scoreBoard);
+let userScore = 0;
+let compScore = 0;
+let userScoreP = document.createElement('p');
+let compScoreP = document.createElement('p');
+scoreBoard.appendChild(compScoreP);
+scoreBoard.appendChild(userScoreP);
+
+
+function endGame() {
+    if (userScore === 5){
+        alert(`User scored ${userScore} points, the game is over`);
+    }
+    else if (compScore === 5){
+        alert(`Comp scored ${compScore} points, the game is over`)
+    }
+}
+
 
 function assigner() {
 
@@ -15,54 +36,57 @@ function assigner() {
     return comp_hand.toLowerCase();
     };
 
-function play_round() {
+function play_round(choice) {
 
-    let sign = prompt('Enter your choice');
-    user_choice = sign.toLowerCase();
+    user_choice = choice.toLowerCase();
     var comp_hand = assigner();
-    console.log('comp_hand = ', comp_hand);
+    var para = document.createElement('p')
+    endGame();
 
     if (user_choice === comp_hand) {
-        alert('DRAW')
-        console.log('DRAW')
+        para.textContent = '\n DRAW'
+        
     }
     else if (user_choice === 'rock' && comp_hand === 'scissors'){
-        alert('User wins with rock')
-        return console.log('User wins with rock')
-        
+        para.textContent = '\n user wins with rock';
+        userScore += 1;
     }
     else if (user_choice === 'paper' && comp_hand === 'rock') {
-        alert('User wins with paper')
-        return console.log('User wins with paper')
-        
+        para.textContent = ('\n User wins with paper');
+        userScore += 1;
     }
     else if (user_choice === 'scissors' && comp_hand === 'paper') {
-        alert('user wins with scissors')
-        return console.log('user wins with scissors')
-       
+        para.textContent = ('User wins with scissors');
+        userScore += 1;
     }
     else if (comp_hand === 'rock' && user_choice === 'scissors'){
-        alert('Computer wins with rock')
-        return console.log('Computer wins with rock')
-        
+        para.textContent = ('\n comp wins with rock');
+        compScore += 1;
     }
     else if (comp_hand === 'paper' && user_choice === 'rock') {
-        alert('Computer wins with paper')
-        return console.log('Computer wins with paper')
-        
+        para.textContent = ('\n Comp wins with paper');
+        compScore += 1;
     }
     else if (comp_hand === 'scissors' && user_choice === 'paper') {
-        alert('Compouter wins with scissors')
-        return console.log('Computer wins with scissors')
-        
+        para.textContent = ('\n comp wins with scissors');
+        compScore += 1;
     }
+    results.appendChild(para);
+    compScoreP.textContent = `comp score = ${compScore}`;
+    userScoreP.textContent = `user score = ${userScore}`;
+
 }
 
-function game() {
-    for (i = 1; i <= 5; i++){
-        play_round()
-    
-    }
-}
+const rock = document.querySelector('.Rock');
+const paper = document.querySelector('.Paper');
+const scissors = document.querySelector('.Scissors');
 
-game()
+rock.addEventListener('click', () => {
+    play_round('Rock')
+})
+paper.addEventListener('click', () => {
+    play_round('Paper');
+})
+scissors.addEventListener('click', () => {
+    play_round('Scissors')
+})
